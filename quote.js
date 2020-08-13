8b857c4117aa62eb1ff5730aef008d9fab84cddb
spininelfianco <69510405+spininelfianco@users.noreply.github.com>
2020-08-14 00:46:09 +0200

    Update quote.js

--- quote.js
+++ quote.js
@@ -115,12 +115,12 @@ $(function() {
       var num = result[x];
 
       if (quotes[num].link == '#') {
-        htmlOutput = '<p>' + decodeURIComponent(quotes[num].quote) + '</p>' + '<footer><a href="#" class="brackets author">' + quotes[num].author + '</a><span class="cursor blink">&#9646;</span></footer>';
+        htmlOutput = '<p>' + quotes[num].quote + '</p>' + '<footer><a href="#" class="brackets author">' + quotes[num].author + '</a><span class="cursor blink">&#9646;</span></footer>';
       } else {
-        htmlOutput = '<p>' + decodeURIComponent(quotes[num].quote) + '</p>' + '<footer><a href="' + quotes[num].link + '" target="_blank" class="brackets author">' + quotes[num].author + '</a><span class="cursor blink">&#9646;</span></footer>';
+        htmlOutput = '<p>' + quotes[num].quote + '</p>' + '<footer><a href="' + quotes[num].link + '" target="_blank" class="brackets author">' + quotes[num].author + '</a><span class="cursor blink">&#9646;</span></footer>';
       }
       $('#' + num).addClass('opened');
-      $tweet.html('<a href="https://twitter.com/intent/tweet?text=' + quotes[num].quote + '-' + quotes[num].author + '" class="btn" target="_blank">Tweet Quote</a>');
+      $tweet.html('<a href="https://twitter.com/intent/tweet?text=' + encodeURIComponent(quotes[num].quote) + '-' + quotes[num].author + '" class="btn" target="_blank">Tweet Quote</a>');
     } else {
       htmlOutput = '<div class="warning"><span>WARNING</span><p> No more new quotes <span class="cursor blink">&#9646;</span></p></div>';
     }
