88408575b56e945cbb61cec390a6a5523abb0e18
SpiniNelFianco <69510405+spininelfianco@users.noreply.github.com>
2022-07-26 07:54:20 +0200

    Add files via upload

---
+++ RadioSpini/config.js
@@ -0,0 +1,25 @@
+/*
+|--------------------------------------------------------------------------
+| Configuration
+|--------------------------------------------------------------------------
+|
+| To configure streaming, define the name of your radio, url and 
+| type of streaming.
+|
+*/
+
+var settings = {
+    'radio_name': 'Radio SpiniNelFianco',
+
+    'url_streaming': 'https://stream-45.zeno.fm/rcu55zb1sxhvv',
+
+    'streaming_type': 'icecast',
+
+    'api_key': 'xxxxxx',
+
+    'historic': true,
+
+    'next_song': false,
+
+    'default_cover_art': 'img/main.png',
+};
